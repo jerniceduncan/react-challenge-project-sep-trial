66207eefa748fc1ad7d688ece7c1a01de3b66edf
jerniceduncan <jerniceduncan@icloud.com>
2021-11-01 14:38:06 -0700

    feat(orderForm,orderList) Add edit and delete order functionality

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { SERVER_IP } from "../../private";
 
 const OrdersList = (props) => {
-  const { orders } = props;
+  const { orders, setOrders } = props;
   if (!props || !props.orders || !props.orders.length)
     return (
       <div className="empty-orders">
         <h2>There are no orders to display</h2>
       </div>
     );
+  const deleteOrder = (orderId) => {
+    fetch(`${SERVER_IP}/api/delete-order`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: orderId,
+      }),
+    })
+      .then((res) => {
+        setOrders(orders.filter((order) => order._id !== orderId));
+      })
+      .catch((error) => console.log(error));
+  };
 
   return orders.map((order) => {
     const createdDate = new Date(order.createdAt);
@@ -23,12 +39,22 @@ const OrdersList = (props) => {
           <p>Ordered by: {order.ordered_by || ""}</p>
         </div>
         <div className="col-md-4 d-flex view-order-middle-col">
-          <p>Order placed at <span id="orderTime">{`${time.hour.toString().padStart(2, "0")}:${time.minute}:${time.seconds}`}</span></p>
+          <p>
+            Order placed at{" "}
+            <span id="orderTime">{`${time.hour.toString().padStart(2, "0")}:${
+              time.minute
+            }:${time.seconds}`}</span>
+          </p>
           <p>Quantity: {order.quantity}</p>
         </div>
         <div className="col-md-4 view-order-right-col">
           <button className="btn btn-success">Edit</button>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            onClick={() => deleteOrder(order._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>{" "}
         </div>
       </div>
     );
