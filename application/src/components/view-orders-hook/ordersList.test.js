6150b848e5ecc53469d3aeefe270f61c2712b926
jerniceduncan <jerniceduncan@icloud.com>
2021-11-01 14:41:54 -0700

    feat(orderForm,orderList) Add edit and delete order functionality

--- application/src/components/view-orders-hook/ordersList.test.js
+++ application/src/components/view-orders-hook/ordersList.test.js
@@ -1,58 +1,60 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-import OrdersList from './ordersList';
+import OrdersList from "./ordersList";
 
-describe('Orders List', () => {
-    test('renders with no prop', () => {
-        render(
-            <OrdersList
-            />
-        )
-        const emptyDiv = document.getElementsByClassName('empty-orders');
-        const arr = Array.from(emptyDiv);
-        expect(arr.length).toBe(1);
-    });
+describe("Orders List", () => {
+  test("renders with no prop", () => {
+    render(<OrdersList />);
+    const emptyDiv = document.getElementsByClassName("empty-orders");
+    const arr = Array.from(emptyDiv);
+    expect(arr.length).toBe(1);
+  });
 
-    test('renders one order', () => {
-        const orders = [
-            {
-                order_item: "Food",
-                quantity: "777",
-                _id: 1
-            }
-        ];
-        render(
-            <OrdersList
-                orders={orders}
-            />
-        )
-        expect(screen.getByText('Food')).toBeInTheDocument();
-        expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
-    });
+  test("renders one order", () => {
+    const orders = [
+      {
+        order_item: "Food",
+        quantity: "777",
+        _id: 1,
+      },
+    ];
+    render(<OrdersList orders={orders} />);
 
-    test('renders multiple orders', () => {
-        const orders = [
-            {
-                order_item: "Food",
-                quantity: "777",
-                _id: 1
-            },
-            {
-                order_item: "Drink",
-                quantity: "888",
-                _id: 2
-            }
-        ];
-        render(
-            <OrdersList
-                orders={orders}
-            />
-        )
-        expect(screen.getByText('Food')).toBeInTheDocument();
-        expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
-        expect(screen.getByText('Drink')).toBeInTheDocument();
-        expect(screen.getByText(/^.*888.*$/gm)).toBeInTheDocument();
+    expect(screen.getByText("Food")).toBeInTheDocument();
+    expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
+  });
 
-    });
-})
+  test("renders multiple orders", () => {
+    const orders = [
+      {
+        order_item: "Food",
+        quantity: "777",
+        _id: 1,
+      },
+      {
+        order_item: "Drink",
+        quantity: "888",
+        _id: 2,
+      },
+    ];
+
+    render(<OrdersList orders={orders} />);
+    expect(screen.getByText("Food")).toBeInTheDocument();
+    expect(screen.getByText(/^.*777.*$/gm)).toBeInTheDocument();
+    expect(screen.getByText("Drink")).toBeInTheDocument();
+    expect(screen.getByText(/^.*888.*$/gm)).toBeInTheDocument();
+  });
+  test("renders correct time", () => {
+    const time = [
+      {
+        createdAt: "2021-10-22T22:26:14.649Z",
+        _id:1,
+      },
+    ];
+    render(<OrdersList orders={time} />);
+    const orderTime = document.getElementById('orderTime');
+    expect(orderTime).toHaveTextContent('03:26:14'); 
+  
+  });
+});
--- application/src/redux/store.js
+++ application/src/redux/store.js
@@ -1,7 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
+import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import ReduxThunk from "redux-thunk";
+import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+const store = createStore(
+  reducers, (applyMiddleware(ReduxThunk))
+);
 
 export default store;
